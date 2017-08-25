d7699d2f04e74b5b8d67388d38aa3a6e5b35a6f2
Steven Y <stevenyildirim@gmail.com>
2017-08-25 09:30:29 -0400

    My solution to Assignment 1.

--- server.js
+++ server.js
@@ -4,11 +4,24 @@ var http = require('http'),
     port = 8080;
 
 /* Global variables */
-var listingData, server;
+var listingData, server, length;
 
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
+  if(parsedUrl.pathname === "/listings") {
+    response.writeHead(200, {
+      'Content-Type':'application/json',
+        'Content-Length' : length,
+        'Access-Control-Allow-Origin':'*'
+    });
+    response.write(listingData);
+    response.end();
 
+  } else {
+    response.writeHead(404, {'Content-Type':'text/plain'});
+    response.write('Bad gateway error');
+    response.end();
+  }
   /*
     Your request handler should send listingData in the JSON format if a GET request 
     is sent to the '/listings' path. Otherwise, it should send a 404 error. 
@@ -18,7 +31,14 @@ var requestHandler = function(request, response) {
    */
 };
 
+//created a server to test because I don't know
+server = http.createServer(requestHandler);
+
 fs.readFile('listings.json', 'utf8', function(err, data) {
+  if(err) throw err;
+  listingData = data;
+  length = Buffer.byteLength(data);
+  server.listen(8080);
   /*
     This callback function should save the data in the listingData variable, 
     then start the server. 
