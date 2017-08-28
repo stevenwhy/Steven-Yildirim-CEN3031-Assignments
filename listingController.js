c6d75e06d6fcba46a7d98dc7099f298d72627dcd
Steven Y <stevenyildirim@gmail.com>
2017-08-28 09:26:28 -0400

    Completed solution for Assignment2

--- listingController.js
+++ listingController.js
@@ -7,8 +7,19 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function(newListing) {
+      // Clone the new listing object so that changes to the model in the form
+      // do not affect previously-submitted instances.
+      var clonedListing = Object.assign({}, newListing);
+
+      $scope.listings.push(clonedListing); 
+      //push lets me send the information found in clonedListing object into the listings 
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1);
+    };
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.listings[index];
+    };
   }
 ]);
