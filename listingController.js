54d4f9c9243e7cae894fd1390317015c14af1628
Steven Y <stevenyildirim@gmail.com>
2017-09-06 17:55:05 -0400

    Made listing codes unique and used it to search for deletion

--- listingController.js
+++ listingController.js
@@ -8,18 +8,29 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       as described in the assignment spec. 
      */
     $scope.addListing = function(newListing) {
+      
+      for(var i = 0; i < $scope.listings.length; i++) {
+        if(newListing.code == Listings[i].code)
+          return;
+      }
       // Clone the new listing object so that changes to the model in the form
       // do not affect previously-submitted instances.
       var clonedListing = Object.assign({}, newListing);
 
       $scope.listings.push(clonedListing); 
-      //push lets me send the information found in clonedListing object into the listings 
+        //push lets me send the information found in clonedListing object into the listings 
+      
     };
-    $scope.deleteListing = function(index) {
-      $scope.listings.splice(index, 1);
+    $scope.deleteListing = function(listing) {
+      for(var i = 0; i < $scope.listings.length; i++) {
+        if(listing.code == $scope.listings[i].code) {
+          $scope.listings.splice(i, 1);
+          return;
+        }
+      }
     };
-    $scope.showDetails = function(index) {
-      $scope.detailedInfo = $scope.listings[index];
+    $scope.showDetails = function(listing) {
+      $scope.detailedInfo = listing;
     };
   }
 ]);
